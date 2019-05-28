aad4a3c5e05ce42f1c0222c8f6f265de680729af
mixampo <maxim_v_e@hotmail.com>
2019-05-28 21:24:12 +0200

    added WebSecurityConfig file

--- src/main/java/rest/security/WebSecurityConfig.java
+++ src/main/java/rest/security/WebSecurityConfig.java
@@ -1,4 +1,18 @@
 package rest.security;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
+public class WebSecurityConfig extends GlobalMethodSecurityConfiguration {
+
 }
+
